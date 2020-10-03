d24d64dad3249c1ca830c0e32a1e30a94d55d8ae
Tyler Walje <walje15@gmail.com>
2020-10-03 12:21:44 -0600

    upload 5103

--- PyPoll1/main.py/file2.py
+++ PyPoll1/main.py/file2.py
@@ -13,8 +13,15 @@ with open(electionresults, newline - "") as csvfile:
         totalvotes = 0
         toalvotes += 1
 
+#lists and counters
 candidates = []
-percentageofvotes = 0
+percentageofvotes = []
 votes = 0
 totalvotes = 0
+
+if row[] not in candidates
+    candidates.append()
+else:
+    pass candidates.append()
+
 
