1785fe75193cc6dad3b087d151b0a6046b19798a
Tyler Walje <walje15@gmail.com>
2020-10-02 18:10:45 -0600

    upload3102

--- PyBank1/Resources/file1.py
+++ PyBank1/Resources/file1.py
@@ -3,7 +3,20 @@ import csv
 
 file1 = os.path.join("budget_data_csv")
 
-with open(file1, 'r') as text:
+with open(file1, newline = " ")
+
+    csvreader = csv.reader(file1, delimiter = ",")
+
+for row in csvreader
+total.append(row[1])
+totalmonths.append
+total = (firstrow[1])
+value = (firstrow[1])
+dates.append(row[1])
+
+
+
+#with open(file1, 'r') as text:
 
     print(text)
 
@@ -27,8 +40,8 @@ with open(file1, 'r') as text:
         for row in csvreader
             if row [0]
 
-months = 0
-value = 0
+months = []
+value = []
 dates = []
 profit = []
 
