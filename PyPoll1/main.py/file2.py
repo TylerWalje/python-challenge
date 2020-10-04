6fd98ea53d892763c2472ad46368055fa04b1666
Tyler Walje <walje15@gmail.com>
2020-10-04 17:22:27 -0600

    upload 4104

--- PyPoll1/main.py/file2.py
+++ PyPoll1/main.py/file2.py
@@ -12,7 +12,6 @@ with open(electionresults, newline - "") as csvfile:
 
     for row in csvreader:
         #counting total amount of votes and adding to the votes
-        totalvotes = 0
         toalvotes += 1
 
 #lists and counters
@@ -21,14 +20,28 @@ percentageofvotes = []
 votes = 0
 totalvotes = 0
 
-if row[] not in candidates
-    candidates.append()
-    index = candidates.index(row[])
+#look for names, add names and add votes
+if row[2] not in candidates
+    candidates.append(row[2])
+    index = candidates.index(row[2])
     num.votes.append(1)
 else:
-    pass candidates.append()
+    index = candidates.append(row[2])
     num.votes[index] += 1
+
+for votes1 in votes
+    percent = (votes1/totalvotes) *100
+    percent = round(percent)
+    percent = "%.3f%%" % percent
+    percentageofvotes.append(percent)
+
+#election winner
+winner = max(votes)
+index = votes.index(winner)
+electionwinner = candidates[index]
+
 
 #printing the info
 print("Election Results")
 print("Total Votes: {}")
+print
