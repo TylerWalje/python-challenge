a5961bacd007232416832612c23916c514e776d3
TylerWalje <walje15@gmail.com>
2024-04-11 14:06:59 -0600

    adjusted pypoll

--- PyPoll/main.py
+++ PyPoll/main.py
@@ -4,24 +4,22 @@ import os
 # Create variables
 votes = 0
 candidates = {}
-winner = ""
-winningvotes = 0
 
 # Csv path
 csv_path = os.path.join('PyPoll', 'Resources', 'election_data.csv')
 
 with open(csv_path, newline='') as csvfile:
     csvreader = csv.reader(csvfile, delimiter=',')
-    next(csvreader)
+    next(csvreader)  
 
-for row in csvreader:
-        votes += 1
-        candidate_name = row[2]
+    for row in csvreader:
+        votes += 1 
+        candidate_name = row[2] 
 
-    if candidate_name in candidates:
+        if candidate_name in candidates:
             candidates[candidate_name] += 1
-    else:
-
+        else:
+            candidates[candidate_name] = 1
 
 
 # Output to text file in folder
