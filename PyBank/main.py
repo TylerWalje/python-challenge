a13c0efbeb8e2644d8a76c719371e73c5a256ffa
TylerWalje <walje15@gmail.com>
2024-04-11 08:34:39 -0600

    variables and csv

---
+++ PyBank/main.py
@@ -0,0 +1,12 @@
+import csv
+
+# Create variables
+months = 0
+total = 0
+previous = None
+monthlychange = []
+increase = ['', 0]
+decrease = ['', 0]
+
+# csv path
+csv_path = 'PyBank/Resources/budget_data.csv'
