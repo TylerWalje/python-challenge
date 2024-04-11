deff6d5134da3d9ab9cf6b0234b6f491d0f7a892
TylerWalje <walje15@gmail.com>
2024-04-11 13:44:39 -0600

    made indentation changes to both

--- PyBank/main.py
+++ PyBank/main.py
@@ -21,22 +21,22 @@ with open(csv_path, newline='') as csvfile:
         months += 1
         total += int(row[1])
 
-        # Monthly change
-        if previous is not None:
+    # Monthly change
+    if previous is not None:
             change = int(row[1]) - previous
             monthlychange.append(change)
 
-            # Profits increace
-            if change > increase[1]:
+    # Profits increace
+    if change > increase[1]:
                 increase = [row[0], change]
 
-            # Profits decrease
-            if change < decrease[1]:
+    # Profits decrease
+    if change < decrease[1]:
                 decrease = [row[0], change]
 
-        previous = int(row[1])
+                previous = int(row[1])
 
-        # average change
+# average change
 average_change = sum(monthlychange) / len(monthlychange) if monthlychange else 0
 
 # Output
@@ -53,7 +53,6 @@ summary = (
 print(summary)
 
 # Output to text file in folder
-output_path = 'PyBank/analysis/financial_analysis_summary.txt'
-with open(output_path, 'w') as file:
+pathout = 'PyBank/analysis/financial_analysis_summary.txt'
+with open(pathout, 'w') as file:
     file.write(summary)
-    
--- PyPoll/main.py
+++ PyPoll/main.py
@@ -9,3 +9,22 @@ winningvotes = 0
 
 # Csv path
 csv_path = os.path.join('PyPoll', 'Resources', 'election_data.csv')
+
+with open(csv_path, newline='') as csvfile:
+    csvreader = csv.reader(csvfile, delimiter=',')
+    next(csvreader)
+
+for row in csvreader:
+        votes += 1
+        candidate_name = row[2]
+
+    if candidate_name in candidates:
+            candidates[candidate_name] += 1
+    else:
+
+
+
+# Output to text file in folder
+# pathout = os.path.join('PyPoll', 'analysis', 'election_results_summary.txt')
+# with open(pathout, 'w') as file:
+   # file.write(summary)
